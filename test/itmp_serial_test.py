92ab07b90b456c156bc76c1991f511e44734c853
kuzya-lakomkin <s.simakov@g.nsu.ru>
2025-07-27 13:45:06 +0700

    removed unused

--- test/itmp_serial_test.py
+++ test/itmp_serial_test.py
@@ -1,12 +1,10 @@
+import sys
+import os
+
+# Добавляем абсолютный путь к ../code
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'utils')))
+
 from utils import itmp_serial
 
 
-def test_itmp2hdlc():
-    addr = 4
-    packet1 = [itmp_serial.ITMPMessageType.CALL, 1, ""]
-    res = itmp_serial.build_hdlc_from_itmp(4, packet1)
 
-    print(res)
-
-
-print(test_itmp2hdlc())
