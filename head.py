f5ec60681f4e8aa418a0d2c0b627c57db7bda590
aloegarten1 <s.simakov@g.nsu.ru>
2025-08-01 03:26:32 -0700

    debug info

--- head.py
+++ head.py
@@ -35,7 +35,6 @@ def poll(serial_port):
 	while (not buffer) and (end - start < timeout):
 		print("Listening to COM...")
 		buffer = serial_port.read(1024)
-		print(buffer)
 		end = time.time()
 	
 	if (end - start > timeout):
@@ -46,7 +45,7 @@ def poll(serial_port):
 
 def process_all_messages(port: serial.Serial, script: dict):
 	'''
-			for message in script["script"]:
+	for message in script["script"]:
 		curr_msg = script[f"msg{0}"]
 		type = curr_msg["type"]
 		build_func = getattr(itmp_serial, f"build_itmp_hdlc_{type}_packet")
@@ -71,18 +70,14 @@ def process_all_messages(port: serial.Serial, script: dict):
 
 		frame = build_func(**curr_msg)
 		print(f"Sending HDLC frame: {frame}")
-		# port.write(frame)
-		# res = poll(port)
-		# print(res)
+		print(port.write(frame))
+		res = poll(port)
 
 def main():
-	print(itmp_serial.build_hdlc_from_itmp(4, [itmp_serial.ITMPMessageType.DESCRIBE, 1, ""]))
-	print(itmp_serial.read_itmp_hdlc_packet(b'\x04\x83\x06\x01`\xf7~'))
 	script_path = "./resources/script1.json"
 
 	print("Head controler launched.")
 	
-	'''
 	port_name = set_port()
 	if not port_name:
 		print("Head controler was finalized.")
@@ -90,9 +85,6 @@ def main():
 	
 	port = com.open_serial_port(port_name)
 	print(f"Using port: {port_name}")
-	'''
-
-	port = None
 
 	with open(script_path, "r") as fin:
 		script = json.load(fin)
